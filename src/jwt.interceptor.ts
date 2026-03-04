dc020a35c5e6617823f530b821ec3b20c735f68e
rhaaf <rhaaf.project@gmail.com>
2026-03-04 23:25:32 +0700

    checkpoint: login fix, UI update to 173 style, JWT interceptor enabled, DB schema fixes

--- src/jwt.interceptor.ts
+++ src/jwt.interceptor.ts
@@ -5,15 +5,15 @@ import { AppService } from './app/service/app.service';
 
 @Injectable({ providedIn: 'root' })
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private service: AppService) {}
+    constructor(private service: AppService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // const bearerToken = this.service.get_localstorage('auth_token');
-        // if (bearerToken) {
-        //     req = req.clone({
-        //         headers: req.headers.set('Authorization', `Bearer ${bearerToken}`),
-        //     });
-        // }
+        const bearerToken = localStorage.getItem('auth_token');
+        if (bearerToken) {
+            req = req.clone({
+                headers: req.headers.set('Authorization', `Bearer ${bearerToken}`),
+            });
+        }
 
         return next.handle(req);
     }
